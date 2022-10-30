4904b0f2b7730abe15bd55a40c75dd5f09a81feb
j. jillian <judith.mediosdigitales@gmail.com>
2022-10-30 09:05:30 +0100

    Set up

---
+++ src/pages/ApartmentsPage.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import React from "react";
+
+function ApartmentsPage() {
+  return (
+    <div>
+      <h3>List Of Apartments</h3>
+    </div>
+  );
+}
+
+export default ApartmentsPage;
